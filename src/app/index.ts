e192f2b4ddbdb40f19f043e6f0928db79c9fe3ee
yasserBelatreche <yasser.belatreche0@gmail.com>
2022-03-04 15:03:05 +0100

    bussiness logic done

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,20 @@
+import {ActivitiesServiceFacade} from './UseCases/ActivitiesService/ActivitiesService';
 import {AuthServiceFacade} from './UseCases/AuthService/AuthServiceFacade';
 
+import {FakeActivitiesGateway} from './__tests__/__fakes__/Persistence/FakeActivitiesGateway';
+import {FakeActivitiesJoiningRequestsGateway} from './__tests__/__fakes__/Persistence/FakeActivitiesJoiningRequestsGateway';
 import {FakeStudentsGateway} from './__tests__/__fakes__/Persistence/FakeStudentsGateway';
 
-const authService = new AuthServiceFacade(new FakeStudentsGateway());
+const studentsGateway = new FakeStudentsGateway();
+const activitiesGateway = new FakeActivitiesGateway();
+const activitiesJoiningRequestsGateway =
+  new FakeActivitiesJoiningRequestsGateway();
 
-export {authService};
+const authService = new AuthServiceFacade(studentsGateway);
+const activitiesService = new ActivitiesServiceFacade(
+  activitiesGateway,
+  activitiesJoiningRequestsGateway,
+  studentsGateway,
+);
+
+export {authService, activitiesService};
